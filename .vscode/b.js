61267dc2c4c4695057a64831be5180bc5b7852d1
Nishant <kuulnishant123@gmail.com>
2021-09-23 23:41:36 +0530

    B

---
+++ .vscode/b.js
@@ -0,0 +1,24 @@
+//compounding assignment with augmented addition and substraction
+ 
+let a = 3;
+let b = 17;
+let c = 99;
+
+a += 25;
+b += 9;
+c -= 99;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//compounding assignment with augmented multiplication and division
+
+let x = 4.6;
+let y = 108;
+
+x *= 10;
+y /= 12;
+
+console.log(x);
+console.log(y);
