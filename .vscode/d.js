0e5f72e34084938d5f3a4162a7a4966a9f2ebe68
Nishant <kuulnishant123@gmail.com>
2021-09-24 01:27:22 +0530

    D

---
+++ .vscode/d.js
@@ -0,0 +1,50 @@
+//finding length of string
+
+var nameLength = 0;
+var Name = "crystal";
+nameLength = Name.length;
+console.log(nameLength);
+
+//bracket notation to find first character in string
+
+var firstLetterOfName = "";
+var Name = "crystal";
+firstLetterOfName = Name[0];
+console.log(firstLetterOfName);
+
+//string immutability
+
+let myStr = "jello world";
+myStr = "hello world";
+console.log(myStr);
+
+//bracket notation to find nth character in string
+
+let firstName = "Nishant";
+let thirdCharacterOfString = firstName[2];
+console.log(thirdCharacterOfString);
+
+//bracket notation to find last character in string
+
+let FirstName = "abraham";
+let LastLetterOfFirstName = FirstName[FirstName.length-1];
+console.log(LastLetterOfFirstName);
+
+
+let eMail = 'kuulnishant123@hotmail.com';
+console.log(eMail);
+
+//string concatenation
+
+let FirstnamE = "Bob";
+let LastnamE = "Marlo";
+let FullName = FirstnamE  + " " +  LastnamE;
+
+console.log(FullName);
+console.log(FullName[2]);
+console.log(FullName.length);
+console.log(FullName.toUpperCase());
+console.log(FullName.toLowerCase());
+
+
+
