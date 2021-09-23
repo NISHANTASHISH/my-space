92d7147dd2a883b33ac20a2a7719b92f94d83919
Nishant <kuulnishant123@gmail.com>
2021-09-24 00:01:33 +0530

    C

---
+++ .vscode/c.js
@@ -0,0 +1,25 @@
+//concatenating string with plus operator
+
+var ourStr = "Sam is a b.tech student. " + "He love painting.";
+console.log(ourStr);
+
+//concatenating strings with plus equal operator
+
+var myStr = "I come first.";
+myStr += " I come second.";
+
+console.log(myStr);
+
+//concatenating strings with variables
+
+let myName = "Tom";
+let Str = "My name is " + myName + " and i am awesome.";
+console.log(Str);
+
+//appending variable to strings
+
+var quality = "Awesome";
+var oStr = "Matt is ";
+oStr += quality;
+console.log(oStr);
+
