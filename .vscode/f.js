5b395146427916af167339246a6c22ecfcb3a25c
Nishant <kuulnishant123@gmail.com>
2021-09-24 21:36:19 +0530

    F

---
+++ .vscode/f.js
@@ -0,0 +1,30 @@
+//booleans
+
+console.log(true,false);
+console.log("true", "false");
+
+let email = 'kuulnishant123@gmail.com';
+let result = email.includes('@');
+console.log(result);
+
+let names = ['mario', 'mona', 'anand'];
+let Result = names.includes('mona');
+console.log(Result);
+
+//comparison operator
+
+let age = 25;
+console.log(age==25);
+console.log(age==32);
+console.log(age<=28);
+console.log(age!=25);
+console.log(age>20);
+
+let person = 'shaun';
+console.log(person == 'shaun');
+console.log(person < 'crystal');
+console.log(person < 'Shaun');
+
+let regNo = 45;
+console.log(regNo === 30);
+console.log(regNo === '45');
